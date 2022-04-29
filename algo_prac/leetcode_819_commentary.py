db5af1e46e1656862f110c648b726bd1c9eabc2e
nakevin96 <nakevin96@hanyang.ac.kr>
2022-04-29 19:12:10 +0900

    잘못 올려서 수정
    잘못 올려서 수정

--- algo_prac/leetcode_819_commentary.py
+++ algo_prac/leetcode_819_commentary.py
@@ -1,27 +1,23 @@
 # 입력값에는 대소문자가 섞여 있으며 쉽표 등 구두점이 존재한다.
 # 따라서 데이터 클렌징이라 부르는 입력값에 대한 전처리 작업이 필요하다.
 # 좀 더 편리하게 처리하기 위해 정규식을 섞어 쓰면 쉽게 처리 가능하다
+import collections
 from typing import List
+import re
 
 
-def reorder_log_files(logs: List[str]) -> List[str]:
-    letters, digits = [], []
-    for log in logs:
-        if log.split()[1].isdigit():
-            digits.append(log)
-        else:
-            letters.append(log)
+def most_common_word(paragraph: str, banned: List[str]) -> str:
+    words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
 
-    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-    return letters+digits
+    counts = collections.Counter(words)
 
-logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
-print(reorder_log_files(logs))
+    print(counts)
+    print(counts.most_common(1))
+    print(counts.most_common(1)[0])
+    return counts.most_common(1)[0][0]
 
 
-"""
-람다 표현식이란 식별자 없이 실행 가능한 함수를 말하며
-함수 선언 없이도 하나의 식으로 함수를 단순하게 표현할 수 있다.
+p = "Bob hit a ball, the hit BALL flew far after it was hit."
+ban = ["hit"]
 
-
-"""
+print(most_common_word(p, ban))
